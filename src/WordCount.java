92abac93d2b3e1df5a208a827ece295441b110fa
Anas Khan <anas.khan@hotmail.co.uk>
2022-05-03 15:45:17 +0100

    first commit

---
+++ src/WordCount.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class WordCount {
+    public static void main (String [] args) throws Exception{
+
+        File file = new File("randomfile.txt");
+        Scanner scanner = new Scanner(file);
+
+        int words = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+
+            words += line.split(" ").length;
+
+        }
+
+        System.out.println("This file contains: " + words + " words");
+
+    }
+
+}
